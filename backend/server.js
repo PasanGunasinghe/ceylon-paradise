c8cc0189028d96272a3e310a77bc5f7a749c66c6
Pasan Gunasinghe <pasan@example.com>
2026-09-06 19:55:29 +0530

    Fix CORS middleware order

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const bcrypt = require('bcryptjs');
 const sharp = require('sharp');
 require('dotenv').config();
